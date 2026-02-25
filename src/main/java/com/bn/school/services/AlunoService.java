6dcc6d24759fa131c6b9968ed8eede9250b22f3c
Leo Teixeira <167496565+Teeixeira@users.noreply.github.com>
2026-02-24 22:00:02 -0300

    Services criados

---
+++ src/main/java/com/bn/school/services/AlunoService.java
@@ -0,0 +1,29 @@
+package com.bn.school.services;
+
+import com.bn.school.models.AlunoModel;
+import com.bn.school.repositories.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlunoService {
+
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    public AlunoModel criarALuno(AlunoModel alunoModel){
+        return alunoRepository.save(alunoModel);
+    }
+
+    public List<AlunoModel> findAll(){
+        return alunoRepository.findAll();
+    }
+
+    public void deletarAluno(Long id){
+        alunoRepository.deleteById(id);
+    }
+
+
+}
